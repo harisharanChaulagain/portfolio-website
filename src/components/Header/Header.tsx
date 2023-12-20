88ea515fa93963c22e56bd772dd409813892c4a8
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-12-20 20:46:01 +0545

    feat: home section design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,23 @@
+import "./Header.scss";
+import { IoReorderThree } from "react-icons/io5";
+import { useState } from "react";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [toggleIcon, setToggleIcon] = useState(false);
+  return (
+    <div className="header-main">
+      <div className="toogle-icon">
+        <IoReorderThree onClick={() => setToggleIcon(!toggleIcon)} />
+      </div>
+      <div className="items-main">
+        <div className={`items ${toggleIcon && "d-none"}`}>Home</div>
+        <div className={`items ${toggleIcon && "d-none"}`}>About</div>
+        <div className={`items ${toggleIcon && "d-none"}`}>Projects</div>
+        <div className={`items ${toggleIcon && "d-none"}`}>Resume</div>
+        <div className={`items ${toggleIcon && "d-none"}`}>Contact</div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,5 +1,23 @@
+import "./Home.scss";
+import image from "../../assets/img1.jpg";
+
 const Home = () => {
-  return <div>this is home</div>;
+  return (
+    <div className="home-main">
+      <div className="first">
+        <h1>Hi, </h1>
+        <h1>My Name is HARI</h1>
+        <h1> SHARAN CHAULAGAIN</h1>
+        <h1></h1>
+        <h2>I'm a Web Developer!!!</h2>
+      </div>
+      <div className="second">
+        <div className="img-container">
+          <img src={image} alt="image" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;

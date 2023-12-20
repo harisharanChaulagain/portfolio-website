65d7dabfc935ab3e5a7ffe964e4c419151bb10b5
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-12-20 14:57:40 +0545

    feat: folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import AboutMe from "./components/AboutMe/AboutMe";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Projects from "./components/Projects/Projects";
+import Resume from "./components/Resume/Resume";
+
 const App = () => {
-  return <div>This is portfolio website</div>;
+  return (
+    <div>
+      <Header />
+      <Home />
+      <AboutMe />
+      <Projects />
+      <Resume />
+      <Contact />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
